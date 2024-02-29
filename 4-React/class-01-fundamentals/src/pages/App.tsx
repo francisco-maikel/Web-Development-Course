4e2e5023864da56992fc693e203d1635a4ab0f9b
Franisco maikel <francisco.gomes664@aluno.ce.gov.br>
2024-02-29 16:15:16 -0300

    add class 01 fundamentals

---
+++ 4-React/class-01-fundamentals/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import logoReact from "../assets/react.svg"
+
+export function App() {
+  return (
+    <>
+    <h1>Class 01 Fundamentals</h1>
+    <p>Syntex TSX = TS + XML</p>
+    <img src="vite.svg" alt="" />
+    <img src={logoReact} alt="" />
+    </>
+  )
+}
